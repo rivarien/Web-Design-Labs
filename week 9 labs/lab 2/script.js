3e8103e2c3226cac7b6bec4ecb3459bb1da60eff
rivarien <jirurivarien@gmail.com>
2024-04-16 20:41:24 +0100

    Buggy prototype
    I need to reduce my rock paper and scissors functions into one function for the round result.
    Current code is too clunky and makes it harder to make three rounds with.

--- week 9 labs/lab 2/script.js
+++ week 9 labs/lab 2/script.js
@@ -1,7 +1,34 @@
-function playGame(){
-var numberOfRounds = 0;
-    while(numberOfRounds<3)
-//unfinished business^^
+//place all previous functions within a new function which, runs the inner functions for three loops max
+function playGame(userChoice){
+    var numberOfRounds = 0;
+    while(numberOfRounds<3){
+
+        //call on the function to generate the computer's choice
+        var cpu = computerOutput();
+
+        //call on the rock paper and scissors functions - parameters for user and computer choices added
+        rock(userChoice, cpu);
+        paper(userChoice, cpu);
+        scissors(userChoice, cpu);
+      
+        //if the player wins, the game stops
+        if(
+            (userChoice=="rock" && cpu=="scissors")||
+            (userChoice=="paper" && cpu=="rock")||
+            (userChoice=="scissors" && cpu=="paper")
+        ){
+            alert("You won in "+(numberOfRounds+1)+" rounds!");
+            return;
+        }
+        
+        //the amount of games played is being tracked
+        numberOfRounds++;
+
+    }
+    //if the user has had three goes, its game over
+    alert("Game over, you played "+numberOfRounds+" rounds. Too bad! :(");  
+}
+
 
 
     function computerOutput(){
@@ -12,40 +39,42 @@ var numberOfRounds = 0;
         } else if(randomNumber == 1){
             return "paper";
         } else{
-            return "scissors";44
+            return "scissors";
         }
     }
 
-    function rock(){
-        var cpu = computerOutput();
-        if(cpu == "rock"){
-            alert("It is a draw, the computer chose " +cpu);
-        } else if(cpu == "paper"){
-            alert("You lose, the computer chose " +cpu);
-        }else{
-            alert("You win!!! The computer chose " +cpu);
+    function rock(userChoice, cpu){
+        if(userChoice == "rock"){
+            if(cpu == "rock"){
+                alert("It is a draw, the computer chose " +cpu);
+            } else if(cpu == "paper"){
+                alert("You lose, the computer chose " +cpu);
+            }else{
+                alert("You win!!! The computer chose " +cpu);
+            }
         }
     }
 
-    function paper(){
-        var cpu = computerOutput();
-        if(cpu == "rock"){
-            alert("You win!!! The computer chose " +cpu);
-        } else if(cpu == "paper"){
-            alert("It is a draw, the computer chose " +cpu);
-        }else{
-            alert("You lose, the computer chose " +cpu);
-        } 
+    function paper(userChoice, cpu){
+        if(userChoice == "paper"){
+            if(cpu == "rock"){
+                alert("You win!!! The computer chose " +cpu);
+            } else if(cpu == "paper"){
+                alert("It is a draw, the computer chose " +cpu);
+            }else{
+                alert("You lose, the computer chose " +cpu);
+            } 
+        }
     }
 
-    function scissors(){
-        var cpu = computerOutput();
-        if(cpu == "rock"){
-            alert("You lose, the computer chose " +cpu);
-        } else if(cpu == "paper"){
-            alert("You win!! The computer chose " +cpu);
-        }else{
-            alert("It is a draw, the computer chose " +cpu);
-        } 
+    function scissors(userChoice, cpu){
+        if(userChoice == "scissors"){
+            if(cpu == "rock"){
+                alert("You lose, the computer chose " +cpu);
+            } else if(cpu == "paper"){
+                alert("You win!! The computer chose " +cpu);
+            }else{
+                alert("It is a draw, the computer chose " +cpu);
+            } 
+        }
     }
-}
