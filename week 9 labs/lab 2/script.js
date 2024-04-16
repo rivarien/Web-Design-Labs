ec99de99a0f28852b5205aa829f1a6bda99b0711
rivarien <jirurivarien@gmail.com>
2024-04-16 21:00:23 +0100

    Efficiency update
    Function for rock paper and scissors is reduced to 1.

--- week 9 labs/lab 2/script.js
+++ week 9 labs/lab 2/script.js
@@ -6,20 +6,8 @@ function playGame(userChoice){
         //call on the function to generate the computer's choice
         var cpu = computerOutput();
 
-        //call on the rock paper and scissors functions - parameters for user and computer choices added
-        rock(userChoice, cpu);
-        paper(userChoice, cpu);
-        scissors(userChoice, cpu);
-      
-        //if the player wins, the game stops
-        if(
-            (userChoice=="rock" && cpu=="scissors")||
-            (userChoice=="paper" && cpu=="rock")||
-            (userChoice=="scissors" && cpu=="paper")
-        ){
-            alert("You won in "+(numberOfRounds+1)+" rounds!");
-            return;
-        }
+        //call on the gameRound function with parameters for user and computer choices added
+        gameRound(userChoice, cpu);
         
         //the amount of games played is being tracked
         numberOfRounds++;
@@ -30,51 +18,39 @@ function playGame(userChoice){
 }
 
 
+ //compute the output for the computer
+function computerOutput(){
+    var randomNumber = Math.floor(Math.random()*3);
 
-    function computerOutput(){
-        var randomNumber = Math.floor(Math.random()*3);
+    if(randomNumber == 0){
+        return "rock";
+    } else if(randomNumber == 1){
+        return "paper";
+    } else{
+        return "scissors";
+    }
+}
 
-        if(randomNumber == 0){
-            return "rock";
-        } else if(randomNumber == 1){
-            return "paper";
-        } else{
-            return "scissors";
-        }
+function gameRound(userChoice, cpu){
+    if(
+        (userChoice=="rock" && cpu=="scissors")||
+        (userChoice=="paper" && cpu=="rock")||
+        (userChoice=="scissors" && cpu=="paper")
+    ){
+        alert("You win!!! The computer chose " +cpu);
     }
 
-    function rock(userChoice, cpu){
-        if(userChoice == "rock"){
-            if(cpu == "rock"){
-                alert("It is a draw, the computer chose " +cpu);
-            } else if(cpu == "paper"){
-                alert("You lose, the computer chose " +cpu);
-            }else{
-                alert("You win!!! The computer chose " +cpu);
-            }
-        }
+    else if(
+        (userChoice=="rock" && cpu=="rock")||
+        (userChoice=="paper" && cpu=="paper")||
+        (userChoice=="scissors" && cpu=="scissors")
+    ){
+        alert("It is a draw, the computer chose " +cpu);
     }
 
-    function paper(userChoice, cpu){
-        if(userChoice == "paper"){
-            if(cpu == "rock"){
-                alert("You win!!! The computer chose " +cpu);
-            } else if(cpu == "paper"){
-                alert("It is a draw, the computer chose " +cpu);
-            }else{
-                alert("You lose, the computer chose " +cpu);
-            } 
-        }
+    else{
+        alert("You lose, the computer chose "+cpu+" :(");
     }
+}
 
-    function scissors(userChoice, cpu){
-        if(userChoice == "scissors"){
-            if(cpu == "rock"){
-                alert("You lose, the computer chose " +cpu);
-            } else if(cpu == "paper"){
-                alert("You win!! The computer chose " +cpu);
-            }else{
-                alert("It is a draw, the computer chose " +cpu);
-            } 
-        }
-    }
+   
