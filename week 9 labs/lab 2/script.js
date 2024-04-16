58da55340a89d7585913240fe4ebe1b833790f3c
rivarien <jirurivarien@gmail.com>
2024-04-16 21:56:38 +0100

    Loop Fix
    Fixed game loop successfully, functions as expected

--- week 9 labs/lab 2/script.js
+++ week 9 labs/lab 2/script.js
@@ -1,21 +1,36 @@
-//place all previous functions within a new function which, runs the inner functions for three loops max
+var numberOfRounds = 0;
+var result = "";
+
 function playGame(userChoice){
-    var numberOfRounds = 0;
-    while(numberOfRounds<3){
+    //the game starts, numberOfRounds incremented
+    numberOfRounds++;
 
-        //call on the function to generate the computer's choice
-        var cpu = computerOutput();
+    //call on the function to generate the computer's choice
+    var cpu = computerOutput();
 
-        //call on the gameRound function with parameters for user and computer choices added
-        gameRound(userChoice, cpu);
-        
-        //the amount of games played is being tracked
-        numberOfRounds++;
+    //call on the gameRound function with parameters for user and computer choices
+    var result = gameRound(userChoice, cpu);
 
+    // Display the result
+    alert(result);
+
+    //Check when the game should end
+    if (result.startsWith("You win")){
+            alert("Congratulations! You won in round #"+numberOfRounds+"!");
+            numberOfRounds=0;
+        }
+    else if(numberOfRounds>=3){
+        alert("Game over, you played for "+numberOfRounds+" rounds, too bad.");
+        numberOfRounds=0;
     }
-    //if the user has had three goes, its game over
-    alert("Game over, you played "+numberOfRounds+" rounds. Too bad! :(");  
+    else{
+        alert("Try again, you're on attempt #"+(numberOfRounds+1)+" of 3.")
+        numberOfRounds+1;
+    }    
 }
+
+   
+
 
 
  //compute the output for the computer
@@ -30,14 +45,14 @@ function computerOutput(){
         return "scissors";
     }
 }
-
+//alert changed to return as the results are being returned to alert(result)
 function gameRound(userChoice, cpu){
     if(
         (userChoice=="rock" && cpu=="scissors")||
         (userChoice=="paper" && cpu=="rock")||
         (userChoice=="scissors" && cpu=="paper")
     ){
-        alert("You win!!! The computer chose " +cpu);
+        return("You win!!! The computer chose " +cpu);
     }
 
     else if(
@@ -45,11 +60,11 @@ function gameRound(userChoice, cpu){
         (userChoice=="paper" && cpu=="paper")||
         (userChoice=="scissors" && cpu=="scissors")
     ){
-        alert("It is a draw, the computer chose " +cpu);
+        return("It is a draw, the computer chose " +cpu);
     }
 
     else{
-        alert("You lose, the computer chose "+cpu+" :(");
+        return("You lose, the computer chose "+cpu+" :(");
     }
 }
 
